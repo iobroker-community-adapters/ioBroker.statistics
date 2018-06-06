9918d62db9aab408893633b3adb35f003c807271
foxthefox <github@wysiwis.net>
2018-06-06 23:07:04 +0200

    Create words.js

---
+++ admin/words.js
@@ -0,0 +1,139 @@
+systemDictionary = {
+
+    "test1":                    {"en": "Test 2", "de": "Test 2", "ru": "Тест 2"},
+    "test2":                    {"en": "Test 2", "de": "Test 2", "ru": "Тест 2"},
+
+
+    "statistics adapter settings": {
+        "en": "statistics adapter settings",
+        "de": "Statistiken Adaptereinstellungen",
+        "ru": "настройки адаптера статистики",
+        "pt": "configurações do adaptador de estatísticas",
+        "nl": "statistieken adapterinstellingen",
+        "fr": "paramètres de l'adaptateur de statistiques",
+        "it": "impostazioni dell'adattatore statistiche",
+        "es": "configuración del adaptador de estadísticas",
+        "pl": "ustawienia adaptera statystyk"
+      },
+      "General": {
+        "en": "General",
+        "de": "Allgemeines",
+        "ru": "Генеральная",
+        "pt": "Geral",
+        "nl": "Algemeen",
+        "fr": "Général",
+        "it": "Generale",
+        "es": "General",
+        "pl": "Generał"
+      },
+      "Meter": {
+        "en": "Meter",
+        "de": "Meter",
+        "ru": "метр",
+        "pt": "Metro",
+        "nl": "Meter",
+        "fr": "Mètre",
+        "it": "metro",
+        "es": "Metro",
+        "pl": "Metr"
+      },
+      "Summen": {
+        "en": "Sums",
+        "de": "Summen",
+        "ru": "Суммы",
+        "pt": "Somas",
+        "nl": "sommen",
+        "fr": "Sommes",
+        "it": "Le somme",
+        "es": "Sumas",
+        "pl": "Sumy"
+      },
+      "Mittelwerte": {
+        "en": "avg value",
+        "de": "durchschnittlicher Wert",
+        "ru": "средняя стоимость",
+        "pt": "valor médio",
+        "nl": "gem. waarde",
+        "fr": "valeur moyenne",
+        "it": "valore medio",
+        "es": "valor medio",
+        "pl": "średnia wartość"
+      },
+      "JSON Import/Export": {
+        "en": "JSON Import/Export",
+        "de": "JSON-Import / Export",
+        "ru": "Импорт / Экспорт JSON",
+        "pt": "JSON Import / Export",
+        "nl": "JSON Import / Export",
+        "fr": "JSON Import / Export",
+        "it": "Importazione / esportazione JSON",
+        "es": "JSON Import / Export",
+        "pl": "Import / eksport JSON"
+      },
+      "sum configuration": {
+        "en": "sum configuration",
+        "de": "Summenkonfiguration",
+        "ru": "сумма конфигурации",
+        "pt": "configuração de soma",
+        "nl": "somconfiguratie",
+        "fr": "configuration de somme",
+        "it": "somma configurazione",
+        "es": "configuración suma",
+        "pl": "konfiguracja sumy"
+      },
+      "avg configuration": {
+        "en": "avg configuration",
+        "de": "durchschnittliche Konfiguration",
+        "ru": "конфигурация avg",
+        "pt": "configuração média",
+        "nl": "avg-configuratie",
+        "fr": "configuration avg",
+        "it": "configurazione AVG",
+        "es": "configuración promedio",
+        "pl": "średnia konfiguracja"
+      },
+      "import/export": {
+        "en": "import/export",
+        "de": "Import Export",
+        "ru": "импорт Экспорт",
+        "pt": "importação / exportação",
+        "nl": "import / export",
+        "fr": "importation / exportation",
+        "it": "import / export",
+        "es": "importación y exportación",
+        "pl": "import Eksport"
+      },
+      "on save adapter restarts with new config immediately": {
+        "en": "on save adapter restarts with new config immediately",
+        "de": "Beim Speichern wird der Adapter sofort mit der neuen Konfiguration neu gestartet",
+        "ru": "на перезаписываемом адаптере перезагружается с новой конфигурацией немедленно",
+        "pt": "on save adapter reinicia com nova configuração imediatamente",
+        "nl": "on save-adapter wordt onmiddellijk opnieuw opgestart met nieuwe config",
+        "fr": "sur l'adaptateur de sauvegarde redémarre avec la nouvelle config immédiatement",
+        "it": "su save adapter si riavvia immediatamente con la nuova configurazione",
+        "es": "en el adaptador de guardar se reinicia con nueva configuración de inmediato",
+        "pl": "na karcie save natychmiast uruchamia się z nową konfiguracją"
+      },
+      "elcost": {
+        "en": "electricity costs",
+        "de": "Stromkosten",
+        "ru": "стоимость электроэнергии",
+        "pt": "custos de eletricidade",
+        "nl": "elektriciteitskosten",
+        "fr": "les coûts d'électricité",
+        "it": "costi dell'elettricità",
+        "es": "costos de electricidad",
+        "pl": "koszty energii elektrycznej"
+      },  
+      "gas costs": {
+        "en": "gas costs",
+        "de": "Gaskosten",
+        "ru": "расходы на газ",
+        "pt": "custos de gás",
+        "nl": "gas kosten",
+        "fr": "les coûts du gaz",
+        "it": "costi del gas",
+        "es": "costos de gas",
+        "pl": "koszty gazu"
+      }
+};
