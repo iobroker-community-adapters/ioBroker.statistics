6e7ed3b0590e6cd65654901483c0f1318d82b72a
foxthefox <github@wysiwis.net>
2019-05-19 22:48:59 +0200

    Update main.js

--- main.js
+++ main.js
@@ -661,8 +661,8 @@ function newTimeCntValue(id, state) {
             callback: (args, callback) => {
                 getValue('temp.timeCount.' + args.id + '.last10', (err, last) => {
                     let delta = last ? state.lc - last : 0;
-		    if (delta < 0) {delta = 0};
-		    else {delta = delta/1000};
+		    if (delta < 0) {delta = 0}
+		    else {delta = delta/1000}
                     setValue('temp.timeCount.' + args.id + '.last01', state.lc, () => {
                         adapter.log.debug('[STATE CHANGE] 0->1 delta ' + delta + ' state ' + state.lc + ' last ' + last);
 
@@ -697,8 +697,8 @@ function newTimeCntValue(id, state) {
             callback: (args, callback) => {
                 getValue('temp.timeCount.' + args.id + '.last01', (err, last) => { 
                     let delta = last ? state.lc - last : 0;
-		    if (delta < 0) {delta = 0};	
-	            else {delta = delta/1000};
+		    if (delta < 0) {delta = 0}	
+	            else {delta = delta/1000}
                     setValue('temp.timeCount.' + args.id + '.last10', state.lc, () => {
                         adapter.log.debug('[STATE CHANGE] 1->0 delta ' + delta + ' state ' + state.lc + ' last ' + last);
 
