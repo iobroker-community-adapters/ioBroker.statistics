7ba5eaee5cbd24f6a0ade531116f7b6dc130533b
Matthias Kleine <github.com9@emailu.de>
2024-08-12 10:15:02 +0200

    eslint

--- main.js
+++ main.js
@@ -545,7 +545,7 @@ class Statistics extends utils.Adapter {
             this.log.info('cleaned everything up...');
 
             callback();
-        } catch (e) {
+        } catch {
             callback();
         }
     }
